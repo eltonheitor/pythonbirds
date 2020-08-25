68ac1dc6ca5f662e5e82ac41460a5110961a6254
elton <eltonheitor@gmail.com>
2020-08-25 04:17:43 -0400

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,14 +1,14 @@
 class Pessoa:
-    def __init__(self, nome=None,idade=35):
+    def __init__(self, *filhos, nome=None,idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p=Pessoa()
-    print(p.cumprimentar())
-    p.nome = 'Elton'
-    print(p.nome)
-    print(p.idade)
+    filho = Pessoa(nome="Heitor")
+    pai   = Pessoa(filho,nome='Elton')
+    for filho in pai.filhos:
+        print(filho.nome)
